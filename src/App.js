3576168b625f2172b0459a65beedd7a3ec3a92f1
Dustin Keyes <dustin.keyes@ibm.com>
2020-10-27 14:19:21 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dustin is super coder, welcome to the future! Show me to your leader
         </p>
         <a
           className="App-link"
